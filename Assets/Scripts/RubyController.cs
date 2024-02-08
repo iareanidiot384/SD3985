a2bab0c3d9a1754f6aadb2de55eaca1a21e2a2c7
iareanidiot384 <shaunleochan2003@gmail.com>
2024-02-08 17:22:45 +0800

    Ws3-Feb8

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class RubyController : MonoBehaviour
 {
+    float horizontal;
+    float vertical;
+    public float movementSpeed = 10;
+    public int hp = 10;
+    public int maxhp = 30;
+
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = this.GetComponent<Rigidbody2D>();
        /* QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 10;*/
     }
@@ -14,11 +23,35 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 3f * horizontal * Time.deltaTime;
-        position.y = position.y + 3f * vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 positionToMove = new Vector2(horizontal, vertical) * movementSpeed * Time.fixedDeltaTime;
+        Vector2 newPos = (Vector2)transform.position + positionToMove;
+
+        rb.MovePosition(newPos);
+    }
+
+    public void hpChange(int value)
+    {
+        hp += value;
+
+        if (hp > maxhp)
+        {
+            hp = maxhp;
+        }
+        if (hp < 0)
+        {
+            hp = 0;
+        }
+
+        hp = Mathf.Clamp(hp, 0, maxhp);
+
+        Debug.Log("Player HP is now: " + hp);
+
+    }
+    
 }
